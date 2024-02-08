84fb3f3310e457ff435e52085c91dd6750ab4273
Zulaigah <zulaigahbenjamin01@gmail.com>
2024-02-08 15:53:25 +0200

    friday

--- script.js
+++ script.js
@@ -1,44 +1,15 @@
 $(document).ready(function () {
-    let currentSlide = 0;
-    const slides = $('.slide');
-    let dotsContainer = $('.dots-container');
-    let dots;
+    $('.slider-inner img:first-child').addClass('active').css('z-index', 10);
+    $('.next').on('click', function () {
+        var currentImg = $('.active');
+        var nextImg = currentImg.next();
 
-    function showSlide(index) {
-        currentSlide = (index + slides.length) % slides.length;
-        slides.css('transform', `translateX(${-100 * currentSlide}%)`);
-        dots.removeClass('active');
-        dots.eq(currentSlide).addClass('active');
-    }
-
-    function changeSlide(offset) {
-        showSlide(currentSlide + offset);
-    }
-
-    $('.next.arrow').on('click', function () {
-        changeSlide(1);
+        if (nextImg.length) {
+            currentImg.removeClass('active').css('z-index', -10);
+            nextImg.addClass('active').css('z-index', 10);
+        } else {
+            $('.slider-inner img').removeClass('active').css('z-index', -10);
+            $('.slider-inner img:first-child').addClass('active').css('z-index', 10);
+        }
     });
-
-    for (let i = 0; i < slides.length; i++) {
-        dotsContainer.append('<div class="dot"></div>');
-    }
-
-    dots = $('.dots-container .dot'); // Move dots assignment inside the document ready
-
-    dotsContainer.on('click', '.dot', function () {
-        const dotIndex = $(this).index();
-        showSlide(dotIndex);
-    });
-
-    showSlide(currentSlide);
 });
-
-function myFunction() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-        x.style.display = "none";
-    } else {
-        x.style.display = "block";
-    }
-}
-
